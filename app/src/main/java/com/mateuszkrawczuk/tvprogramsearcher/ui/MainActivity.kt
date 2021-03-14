c90514727b550ec215ddf6f2c27652d02f369e08
Mateusz Krawczuk <mateusz.krawczuk@onemeter.com>
2021-03-14 23:17:33 +0100

    CleanUp MainActivity
    Signed-off-by: Mateusz Krawczuk <mateusz.krawczuk@onemeter.com>

--- app/src/main/java/com/mateuszkrawczuk/tvprogramsearcher/ui/MainActivity.kt
+++ app/src/main/java/com/mateuszkrawczuk/tvprogramsearcher/ui/MainActivity.kt
@@ -3,22 +3,21 @@ package com.mateuszkrawczuk.tvprogramsearcher.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.mateuszkrawczuk.tvprogramsearcher.BuildConfig
 import org.koin.androidx.compose.getViewModel
 import org.osmdroid.config.Configuration
 
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,16 +25,14 @@ class MainActivity : ComponentActivity() {
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
 
         setContent {
-
             MainLayout()
         }
     }
 }
 
 sealed class Screen(val title: String) {
-    object PersonList : Screen("PersonList")
-    object PersonDetails : Screen("PersonDetails")
-    object ISSPositionScreen : Screen("ISSPosition")
+    object ShowList : Screen("ShowList")
+    object ShowDetails : Screen("ShowDetails")
 }
 
 data class BottomNavigationitem(
@@ -44,63 +41,27 @@ data class BottomNavigationitem(
     val iconContentDescription: String
 )
 
-val bottomNavigationItems = listOf(
-    BottomNavigationitem(
-        Screen.PersonList.title,
-        Icons.Default.Person,
-        "People"
-    ),
-)
-
+@ExperimentalComposeUiApi
 @Composable
 fun MainLayout() {
     val navController = rememberNavController()
 
     val tvProgramSearcherViewModel = getViewModel<TvProgramSearcherViewModel>()
 
-    Scaffold(
-
-        bottomBar = {
-            BottomNavigation {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
-
-                bottomNavigationItems.forEach { bottomNavigationitem ->
-                    BottomNavigationItem(
-                        icon = {
-                            Icon(
-                                bottomNavigationitem.icon,
-                                contentDescription = bottomNavigationitem.iconContentDescription
-                            )
-                        },
-                        selected = currentRoute == bottomNavigationitem.route,
-                        onClick = {
-                            navController.navigate(bottomNavigationitem.route) {
-                                popUpTo = navController.graph.startDestination
-                                launchSingleTop = true
-                            }
-                        }
-                    )
-                }
-            }
-        }
-    ) {
-        NavHost(navController, startDestination = Screen.PersonList.title) {
-            composable(Screen.PersonList.title) {
+    Scaffold {
+        NavHost(navController, startDestination = Screen.ShowList.title) {
+            composable(Screen.ShowList.title) {
                 ShowListScreen(
                     showSelected = {
-                        navController.navigate(Screen.PersonDetails.title + "/${it.name}")
+                        navController.navigate(Screen.ShowDetails.title + "/${it.name}")
                     }
                 )
             }
-            composable(Screen.PersonDetails.title + "/{person}") { backStackEntry ->
+            composable(Screen.ShowDetails.title + "/{person}") { backStackEntry ->
                 ShowDetailsScreen(
                     backStackEntry.arguments?.get("person") as String,
                     popBack = { navController.popBackStack() })
             }
         }
     }
-
-
-
 }
